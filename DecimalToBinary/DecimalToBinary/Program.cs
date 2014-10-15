c302171a689c397999cae5f53f09340976136ec8
Jarrod Thibodeau <thibodeaujz@gmail.com>
2014-10-15 00:45:38 -0400

    Added Decimal To Binary

---
+++ DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This reads every line of the file
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                        continue;
+
+                    int deci = Convert.ToInt32(line); // convert to decimal
+
+                    Console.Out.WriteLine(Convert.ToString(deci, 2)); //print out binary representation 
+                   
+                }
+        }
+    }
+}
