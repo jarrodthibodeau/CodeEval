9eaa319deb26e8d4217579e9042cc9cda6e526b5
jzthibodeau <thibodeaujz@gmail.com>
2014-10-17 13:17:38 -0400

    Added Stack Implementation

--- StackImplementation/src/Main.java
+++ StackImplementation/src/Main.java
@@ -2,55 +2,97 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Stack;
 
+/**
+ *
+ * Stack Implementation Programming Challenge
+ * By Jarrod Z. Thibodeau
+ * October 17th, 2014
+ *
+ * Purpose: Create a stack interface of integers, and push elements into it.
+ * Then for every other element, pop and display the element, otherwise just pop it.
+ *
+ */
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		File file = new File(args[0]);
-		BufferedReader in = new BufferedReader(new FileReader(file));
-		String line;
-		while ((line = in.readLine()) != null) {
-			String[] lineArray = line.split(" ");
-			if (lineArray.length > 0) {
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String[] lineArray = line.split("\\s");
+            if (lineArray.length > 0) {
 
-				Stack<Integer> firstStack = new Stack<Integer>();
-								
-				for(int i = 1; i < lineArray.length + 1; i++){					
-					firstStack.push(Integer.parseInt(lineArray[i-1]));
-				}
-								
-				Stack<Integer> oddStack = new Stack<Integer>();
-				Stack<Integer> evenStack = new Stack<Integer>();
-				
-				boolean swi = true;
-				
-				while(!firstStack.isEmpty()){
-					
-					if(swi){
-						evenStack.push(firstStack.pop());
-						swi = false;
-					}
-					
-					else{
-						oddStack.push(firstStack.pop());
-						swi = true;
-					}
-					
-				}
+                //Creating a new instance of my stack class
+                //Which has the length of the string array
+                Stack myStack = new Stack(lineArray.length);
 
-				
-				for(int i = 0; i <= evenStack.size(); i++){
-					System.out.print(evenStack.pop() + " ");
-				}
-				
-				
+                //Pushing converted ints into my stack
+                for(int i = 0; i < myStack.theSize; i++){
+                    myStack.push(Integer.parseInt(lineArray[i]));
+                }
 
-			}
-			
-			System.out.println("");
+                //For every even stack elements, pop & display,
+                //Otherwise, pop
+                for(int i = 0; i < myStack.theSize; i++){
+                    if(i % 2 == 0)
+                    {
+                        System.out.print(myStack.pop() + " ");
+                    }
 
-		}
-	}
+                    else
+                    {
+                        myStack.pop();
+                    }
+                }
+
+                System.out.println("");
+
+            }
+        }
+    }
+
+
+        /**
+         * Stack class used for implementation
+         * Uses the typical push and pop methods
+         */
+
+        static public class Stack {
+
+            //Attributes
+            private int theSize;
+            private int[] stackArray;
+            private int top;
+
+
+            //Stack constructor that takes an int
+            public Stack(int s) {
+
+                theSize = s;
+                stackArray = new int[theSize];
+                top = -1;
+
+            }
+
+            //Stack push function
+            public void push(int i) {
+                stackArray[++top] = i;
+
+            }
+
+            //Stack pop function
+            public int pop() {
+                return stackArray[top--];
+
+            }
+
+            //Stack peep function
+            public int peek() {
+                return stackArray[top];
+            }
+
+        }
+
+
 }
